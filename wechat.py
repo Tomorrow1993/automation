3dcc8931904e07f54b7873ca51d408c376c028c1
70kg <mrwrong12138@gmail.com>
2017-06-14 23:31:10 +0800

    tt

--- wechat.py
+++ wechat.py
@@ -1,33 +1,39 @@
-#coding=utf8
+# coding=utf8
 import itchat
 import requests
 from bs4 import BeautifulSoup
+
+
 @itchat.msg_register(itchat.content.TEXT)
 def music_player(msg):
     if msg['ToUserName'] != 'filehelper': return
     if msg['Text'].strip() == u'blog':
-		itchat.send(getYWBlog(), 'filehelper')
+        itchat.send(getYWBlog(),'filehelper')
     elif msg['Text'].strip() == u'help':
-		itchat.send(buildHelpMessage(), 'filehelper')
+        itchat.send(buildHelpMessage(), 'filehelper')
     else:
-    	itchat.send(buildHelpMessage(), 'filehelper')
+        itchat.send(buildHelpMessage(), 'filehelper')
 
 
 def getYWBlog():
-	r = requests.get('http://www.yinwang.org')
-	soup = BeautifulSoup(r.text,"html.parser")
-	a = soup.select(".list-group-item a")
-	return u"题目："+a[0].string+"\n"+"http://www.yinwang.org"+a[0].attrs["href"]
+    r = requests.get('http://www.yinwang.org')
+    soup = BeautifulSoup(r.text, "html.parser")
+    a = soup.select(".list-group-item a")
+    return u"题目：" + a[0].string + "\n" + "http://www.yinwang.org" + a[0].attrs["href"]
+
 
 def buildHelpMessage():
-	return u"目前支持功能:\n"+u"blog:--->获取王垠最新博客\n"
+    return u"目前支持功能:\n" + u"blog:--->获取王垠最新博客\n"
 
 
 def lc():
     print('finish login')
+
+
 def ec():
     print('exit')
-    
-itchat.auto_login(hotReload=True,enableCmdQR=False,loginCallback=lc, exitCallback=ec)
-itchat.send(u'输入help,显示帮助信息', 'filehelper') 
+
+
+itchat.auto_login(hotReload=True, enableCmdQR=False, loginCallback=lc, exitCallback=ec)
+itchat.send(u'输入help,显示帮助信息', 'filehelper')
 itchat.run()
