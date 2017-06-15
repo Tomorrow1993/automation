090609342023f1eb30c0a1141515a3b3bc4a37d6
wangpeng <wangp@bolo.me>
2017-06-15 20:26:27 +0800

    t

--- wechat.py
+++ wechat.py
@@ -3,21 +3,29 @@ import itchat
 import net
 import tuling
 
+tuling_anable = False  # 是否开启图灵机器人
+
 
 @itchat.msg_register(itchat.content.TEXT)
 def music_player(msg):
+    global tuling_anable
     if msg['ToUserName'] != 'filehelper': return
     content = msg['Text'].strip()
-    if 'blog' in content:
+    if content == 'start':
+        tuling_anable = True
+        itchat.send(u'已开启图灵机器人', 'filehelper')
+    elif content == 'end':
+        tuling_anable = False
+        itchat.send(u'已关闭图灵机器人', 'filehelper')
+    elif tuling_anable:
+        itchat.send(tuling.tuling(content, msg['ToUserName']), 'filehelper')
+    elif 'blog' in content:
         if 'list' in content:
             itchat.send(net.get_yw_blog_list(), 'filehelper')
         else:
             itchat.send(net.get_yw_blog(), 'filehelper')
     elif content == 'help':
         itchat.send(buildHelpMessage(), 'filehelper')
-    elif 'ask' in content:
-        message = content.split('_', 1)[1]
-        itchat.send(tuling.tuling(message, msg['ToUserName']), 'filehelper')
     else:
         itchat.send(buildHelpMessage(), 'filehelper')
 
@@ -26,7 +34,7 @@ def buildHelpMessage():
     return u"目前支持功能:\n" \
            + u"blog:--->获取王垠最新博客\n" \
            + u"blog list:--->获取王垠所有博客\n" \
-           + u"ask_问题:--->机器人问答"
+           + u"start/end:--->开启/关闭图灵机器人问答"
 
 
 itchat.auto_login(hotReload=True, enableCmdQR=2)
