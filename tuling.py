1bef4a94c322a6979e7436e03687f012f027c474
wangpeng <wangp@bolo.me>
2017-06-15 16:52:23 +0800

    t

--- tuling.py
+++ tuling.py
@@ -11,8 +11,25 @@ def tuling(message, user_id):
     try:
         r = requests.post('http://www.tuling123.com/openapi/api', payload, timeout=8)
         if r.status_code == requests.codes.ok:
-            return json.loads(r.text)['text']
+            try:
+                return hand_response(json.loads(r.text))
+            except (ValueError):
+                return u'万恶的网络又被劫持了'
         else:
             return u'万恶的网络又被劫持了'
     except(requests.exceptions.RequestException):
         return u'万恶的网络又出问题了'
+
+
+def hand_response(response):
+    code = response['code']
+    if code == 100000:  # 文本类
+        return response['text']
+    elif code == 20000:  # 链接类
+        return response['text'] + "\n" + response['url']
+    elif code == 302000:  # 新闻类
+        return response['text']
+    elif code == 308000:  # 菜谱类
+        return response['text']
+    else:
+        return response['text']
