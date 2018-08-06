fb107f0a83e47ee44115fefd1007a81e9ff93dd0
刘畅 <eddie@xingren.com>
2018-08-06 21:11:38 +0800

    更改为127.0.0.1

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from HTMLReport import logger
 
-remote = "http://localhost:4723/wd/hub"
+remote = "http://127.0.0.1:4723/wd/hub"
 
 
 def findelementbyid(self, element):
